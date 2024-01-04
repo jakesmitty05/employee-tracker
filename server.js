f27b564f22bf9deeeb0fbb18979a4e80e8fde6d0
Jakesmitty05 <jakesmitty05@gmail.com>
2024-01-04 02:43:31 -0500

    added gitignore and updated file structure and added to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+);
