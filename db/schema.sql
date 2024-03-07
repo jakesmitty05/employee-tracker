51ff40ea6466511c667d9870b2ffdabaae1e1bde
Jakesmitty05 <jakesmitty05@gmail.com>
2024-03-07 18:57:59 -0500

    updated databse and started working on server.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,29 @@ CREATE DATABASE work_db;
 
 USE work_db;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES roles(id),
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employees(id)
 );
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -18,3 +19,38 @@ const db = mysql.createConnection(
     database: process.env.DB_NAME,
   },
 );
+
+
+
+
+
+
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to see?',
+      name: 'task',
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    },
+  ])
+  .then((response) =>
+    console.log(response.task),
+    db.query('SELECT * FROM departments', function (err, results) {
+      console.log(results);
+    })
+  );
+
+
+
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
