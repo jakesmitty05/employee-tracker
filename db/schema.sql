76845f03273aaacff58d8c1a50290b059d44d728
Jakesmitty05 <jakesmitty05@gmail.com>
2024-03-08 20:09:26 -0500

    started to add sql queries to the user inputs from inquirer and updated package.json to include ctable

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,29 @@ CREATE DATABASE work_db;
 
 USE work_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    REFERENCES department(id)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id),
+    REFERENCES role(id),
     FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
+    REFERENCES employee(id)
 );
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -14,43 +16,126 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    port: PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
 );
 
+// prompt the user with options
+const runPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'task',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+      },
+    ])
+    .then((response) => {
+      // Based on the user's choice, make corresponding MySQL queries
+      switch(response.task) {
+        case 'View all departments':
+          viewAllDepartments();
+        break;
+        case 'View all roles':
+          viewAllRoles();
+        break;
+        case 'View all employees':
+          viewAllEmployees();
+        break;
+        case 'Add a department':
+          addDepartment();
+        break;
+        case 'Add a role':
+          addRole();
+        break;
+        case 'Add an employee':
+          addEmployee();
+        break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+        break;
+      }
+  });
+}
 
 
 
+const viewAllDepartments = () => {
+  db.query(
+    `SELECT * FROM department;`,
+    (err, departments) => {
+      if (err) throw err;
+      const table = cTable.getTable(departments);
+      console.log(table);
+      runPrompt();
+    }
+  )
+}
 
+const viewAllRoles = () => {
+  db.query(
+    `SELECT title, role.id, department.name AS department, salary FROM role 
+    INNER JOIN department ON role.department_id = department.id;`,
+    (err, roles) => {
+      if (err) throw err;
+      const table = cTable.getTable(roles);
+      console.log(table);
+      runPrompt();
+    }
+  )
+}
 
+const viewAllEmployees = () => {
+  db.query(
+    `SELECT employee.id, first_name, last_name, title, name AS department, salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id;`,
+    (err, employees) => {
+      if (err) throw err;
+      const table = cTable.getTable(employees);
+      console.log(table);
+      runPrompt();
+    }
+  )
+}
 
-inquirer
-  .prompt([
+const addDepartment = () => {
+  inquirer.prompt([
     {
-      type: 'list',
-      message: 'What would you like to see?',
-      name: 'task',
-      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+      type: 'input',
+      message: 'What department would you like to add?',
+      name: 'department'
     },
   ])
-  .then((response) =>
-    console.log(response.task),
-    db.query('SELECT * FROM departments', function (err, results) {
-      console.log(results);
-    })
-  );
+  .then((response) => {
+    db.query(
+      `INSERT INTO department (name) VALUES (?)`,
+      [response.department],
+      (err) => {
+        if (err) throw err;
+        console.log('Department added');
+        runPrompt();
+      }
+    )
+  })
+}
 
+const addRole = () => {
+  
+}
 
+const addEmployee = () => {
+  
+}
 
+const updateEmployeeRole = () => {
+  
+}
 
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+runPrompt();
